d521a42b8c5dd4fcc2924a3aefd74841acc02989
johnyedson10 <142899559+johnyedson10@users.noreply.github.com>
2026-05-16 09:51:32 -0300

    Add files via upload

---
+++ introducao.py
@@ -0,0 +1,31 @@
+"""
+verificar a versão do Python
+python --version
+"""
+
+"""
+variaveis
+endereço de memória
+"""
+
+nome1 = "maria silva"
+nome2 = "joão almeida"
+cpf1 = "010.389.111.222"
+
+print(nome1);
+
+"""
+lista de variaveis
+"""
+
+
+"""
+condicionais
+"""
+
+if(cpf1 == ""):
+    ""
+
+"""
+funções e ações
+"""
